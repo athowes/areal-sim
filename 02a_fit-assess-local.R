c2bc7e5193f8b3b18e20511fa6a377f15d2594ff
Adam Howes <adamhowes@outlook.com>
2021-05-03 11:52:59 +0100

    No need to call names()

--- 02a_fit-assess-local.R
+++ 02a_fit-assess-local.R
@@ -1,12 +1,3 @@
-fit_and_assess(
-  geometry = "grid",
-  sim_model = "iid",
-  inf_model = "constant",
-  fn = constant_inla,
-  indices = 1,
-  ctx_ver = "local-test"
-)
-
 # Testing code locally fitting to only a single of the replicates
 
 geometries <- c("grid", "ci", "tex")
@@ -15,8 +6,11 @@ inf_models <- c("constant", "iid", "icar", "bym", "fck", "fik", "ck", "ik")
 fns <- list(constant_inla, iid_inla, besag_inla, bym2_inla, fck_inla, fik_inla, ck_stan, ik_stan)
 indices <- list(1, 1, 1, 1, 1, 1, 1, 1) # Only the first replicate
 
-pars <- expand.grid(geometries, sim_models, inf_models)
-names(pars) <- c("geometry", "sim_model", "inf_model")
+pars <- expand.grid(
+  "geometry" = geometries, 
+  "sim_model" = sim_models, 
+  "inf_model" = inf_models
+)
 
 k <- length(geometries) * length(sim_models)
 pars$fn <- rep(fns, k)
